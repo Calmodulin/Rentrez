623f248bf1c78aeeb5ecfb1c3bad99eefa528582
Calmodulin <you@example.com>
2021-02-12 21:47:10 -0500

    2nd commit

--- COVIDgenome.R
+++ COVIDgenome.R
@@ -10,9 +10,13 @@ COVID3<-gsub(".*genome+","",paste(COVID2,collapse="")) #extract the actual nucle
 COVID3
 
 #Extracting the S protein, which is between 21563 p and 25384 bp 
+#Sorry, I've tried many ways using Regex but nothing pretty had turned out. and frankly i think the following method might be more efficient then using Regex 
 substr(COVID3,21563,25384)
 
 
 #Blast Search and comments: 
 #link to the BLAST result:https://blast.ncbi.nlm.nih.gov/Blast.cgi#alnHdr_1973567854
-#The gene is highly conserved, as it appears in all of our hits. 
+#The gene is highly conserved, as it appears in all of our hit results with perfect matches. 
+#The matches are also conserved across different strains of SARS-CoV-19
+#These findings speculate that the S protein  is essential to all variance of the virus. 
+#Moreove, the role in host infection of S protein ould also suggest that  S protein contribute to the transmissibility of the virus
